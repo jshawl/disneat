9421e842ce830da94a70984087809fc9b97e86ba
Jesse Shawl <jesse@jshawl.com>
2014-09-01 09:25:37 -0400

    add seeds and user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+User.create([
+  { name:"Jesse Shawl", age: 25, height: 72 },
+  { name:"Justin Bieber", age: 20, height: 34 },
+  { name:"Nate Weiner", age: 30, height: 41 },
+  { name:"Jenny Lewis", age: 20, height: 52 },
+  { name:"Marissa Meyer", age: 39, height: 58 }
+])
